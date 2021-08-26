4bfc0d504cb20f336de2edb5e0165c004693bc90
vikaku <39327910+vikaku@users.noreply.github.com>
2021-08-26 13:14:58 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { ProgressBar } from './component/ProgressBar/ProgressBar';
+import Questionaire from './component/Questionair/Questionaire';
+
+const  App = () => {
+  return (
+    <>
+                
+              <ProgressBar />
+              <Questionaire />
+    </>
+  );
+}
+
+export default App;
